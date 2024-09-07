00375206fb24038705324d86856fc1feaf988e8c
Adélio Fonseca <58235074+Adhenrique12@users.noreply.github.com>
2024-09-07 13:51:01 -0100

    Delete marques_bot directory

--- marques_bot/bot/commands.py
+++
@@ -1,5 +0,0 @@
-from telegram.ext import CommandHandler
-
-def register_commands(dispatcher):
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("help", help))
