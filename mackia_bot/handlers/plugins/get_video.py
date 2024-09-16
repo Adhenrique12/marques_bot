8f081943d562c4d0e1c6e3dc0ce4a6503ebcae81
Adhenrique12 <adeliofonseca.lord@hotmail.com>
2024-09-16 18:04:18 -0100

    add select format button

--- mackia_bot/handlers/plugins/get_video.py
+++ mackia_bot/handlers/plugins/get_video.py
@@ -1,4 +1,4 @@
-from telethon import events
+from telethon import events, Button
 from telethon.tl.types import DocumentAttributeVideo
 import os
 from handlers.tools import convert_size, clear, convert_video, download_video, info_video
@@ -13,50 +13,68 @@ async def handler(event):
     entity = await event.get_chat()
     client = event.client
     url = event.pattern_match.group(1)
+    msg_start = await event.reply("Start Download")
+
+    #Select the resoluction of the video
+    @client.on(events.CallbackQuery)
+    async def callback(event):
+        format_id = event.data.decode('utf-8')
+        await client.delete_messages(entity, msg_op)
+        
+        # Download video and get the info(for the name)
+        file_name, info = await download_video.download_video_ydl(url, format_id, event, client, entity)
+
+        # Convert video to MP4 format suitable for Telegram
+        output_file = await convert_video.convert_video(client, entity, msg_start, file_name)
+    
+        # Get info for telegram stream video player
+        width, height, duration = await info_video.info_video(output_file)
+
+        msg = await client.edit_message(entity, msg_start.id, text="Uploading ...")
+
+        async def progress_callback(current, total):
+                message = await client.get_messages(entity, ids=msg.id)
+
+                percent = (current / total) * 100
+                upload_text = None
+
+                if 20.0 < percent < 30.0:
+                    upload_text = f"'Uploaded 1/4 out of', {convert(total)}"
+                elif 50.0 < percent < 60.0:
+                    upload_text = f"'Uploaded 2/4 out of', {convert(total)}"
+                elif 70.0 < percent < 80.0:
+                    upload_text = f"'Uploaded 3/4 out of', {convert(total)}"
+                elif 80.0 < percent < 95.0:
+                    upload_text = f"'Uploaded 4/4 out of', {convert(total)}"
+
+                if upload_text and message.text != upload_text:
+                    await client.edit_message(entity, msg.id, text=upload_text)
+        
+        await client.send_file(event.chat_id, 
+                            output_file, 
+                            caption=info['title'], 
+                            progress_callback=progress_callback,
+                            attributes=(
+                                    DocumentAttributeVideo(
+                                        int(float(duration)),
+                                        width,
+                                        height,
+                                        supports_streaming=True
+                                    ),
+                            )
+                )
+        await client.delete_messages(entity, msg)
+        os.remove(file_name)
+        os.remove(output_file)
+
+
+    resolutions = await download_video.get_video_resolutions(url)
+    buttons = [[Button.inline(res, res.encode('utf-8'))] for res in resolutions]
+    msg_op = await event.respond("Escolha a resolução do vídeo:", buttons=buttons)
+    
+
 
     # Download video and get the info(for the name)
-    video_file, info = await download_video.download_video_ydl(url, event, client, entity)
+    #video_file, info = await download_video.download_video_ydl(url, event, client, entity)
+
     
-
-    # Convert video to MP4 format suitable for Telegram
-    output_file = await convert_video.convert_video(client, entity, msg, video_file)
-    
-    # Get info for telegram stream video player
-    width, height, duration = await info_video.info_video(output_file)
-
-    msg = await client.edit_message(entity, msg.id, text="Uploading ...")
-
-    async def progress_callback(current, total):
-            message = await client.get_messages(entity, ids=msg.id)
-
-            percent = (current / total) * 100
-            upload_text = None
-
-            if 20.0 < percent < 30.0:
-                upload_text = f"'Uploaded 1/4 out of', {convert(total)}"
-            elif 50.0 < percent < 60.0:
-                upload_text = f"'Uploaded 2/4 out of', {convert(total)}"
-            elif 70.0 < percent < 80.0:
-                upload_text = f"'Uploaded 3/4 out of', {convert(total)}"
-            elif 80.0 < percent < 95.0:
-                upload_text = f"'Uploaded 4/4 out of', {convert(total)}"
-
-            if upload_text and message.text != upload_text:
-                await client.edit_message(entity, msg.id, text=upload_text)
-    
-    await client.send_file(event.chat_id, 
-                           output_file, 
-                           caption=info['title'], 
-                           progress_callback=progress_callback,
-                           attributes=(
-                                DocumentAttributeVideo(
-                                    int(float(duration)),
-                                    width,
-                                    height,
-                                    supports_streaming=True
-                                ),
-                           )
-            )
-    await client.delete_messages(entity, msg)
-    os.remove(video_file)
-    os.remove(output_file)
--- mackia_bot/handlers/tools/download_video.py
+++ mackia_bot/handlers/tools/download_video.py
@@ -1,28 +1,48 @@
 import yt_dlp
 
-async def download_video_ydl(url: str, event, client, entity) -> None:
+
+async def get_video_resolutions(url: str):
+    ydl_opts = {
+        'listformats': True,
+    }
+    format_ids = []
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        try:
+            info_dict = ydl.extract_info(url, download=False)
+            formats = info_dict.get('formats', [])
+            for f in formats:
+                format_id = f['format_id']
+                format_ids.append(format_id)
+                print(f"Format ID: {format_id}, Resolution: {f.get('resolution')}, Size: {f.get('filesize') or 'Unknown'}")
+        except yt_dlp.utils.DownloadError as e:
+            print(f"DownloadError: {e}")
+        except yt_dlp.utils.ExtractorError as e:
+            print(f"ExtractorError: {e}")
+        except yt_dlp.utils.PostProcessingError as e:
+            print(f"PostProcessingError: {e}")
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+    return format_ids
+
+
+
+async def download_video_ydl(url: str,format_id: str, event, client, entity):
     """
     Downloads a video from a link
     """
     msg = await event.respond('Downloading video ...')
 
+
     ydl_opts = {
-        'format' : 'best',
+        'format' : format_id,
         'outtmpl' : '%(title)s.%(ext)s',
     }
 
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            info = await ydl.extract_info(url, download=True)
-            return await ydl.prepare_filename(info), info
-        
-        await client.edit_message(entity, msg.id, text="Download successful!")
+            info = ydl.extract_info(url, download=True)
+            filename = ydl.prepare_filename(info)
+            await client.delete_messages(entity, msg)
+            return filename, info
     except Exception as e:
-        await client.edit_message(entity, msg.id, text="Error occurred during download: {str(e)}")
-
-
-        
-
-    
-
-    
+        await client.edit_message(entity, msg.id, text=f"Error occurred during download: {str(e)}")
